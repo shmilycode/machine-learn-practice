184d60fa2d6f58a16c6406685a67b6a30fad28e6
walle <784055830@qq.com>
2019-06-14 23:47:58 +0800

    Add decisionTree plot

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,5 +1,6 @@
 import math
 import copy
+import matplotlib.pyplot as plt
 
 def CreateDataSet():
   dataSet = [[1,1,'yes'],
@@ -99,18 +100,104 @@ def classfy(inputTree, featLabels, testVec):
 
   key = testVec[labelIndex]
   valueOfFeat = childTree[key]
-  print '+++', rootLabel, 'xxx', childTree, '---', key, '>>>', valueOfFeat
+  print('+++', rootLabel, 'xxx', childTree, '---', key, '>>>', valueOfFeat)
   if isinstance(valueOfFeat, dict):
     classLabel = classfy(valueOfFeat, featLabels, testVec)
   else:
     classLabel = valueOfFeat
   return classLabel
 
+def retrieveTree(i):
+  listOfTrees = [{'no surfacing': {0: 'no', 1:{'flippers': \
+    {0: 'no', 1:'yes'}}}},
+    {'no surfacing':{0:'no',1:{'flippers':\
+      {0:{'head':{0:'no',1:'yes'}}, 1:'no'}}}}]
+  return listOfTrees[i]
 
-trainingSet, labels = CreateDataSet()
-tree = createTree(trainingSet, copy.deepcopy(labels))
-testSet = [0,0]
-print(classfy(tree, labels, testSet))
-print(classfy(tree, labels, [1,0]))
-print(classfy(tree, labels, [1,1]))
-print(classfy(tree, labels, [0,1]))
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+leafNode = dict(boxstyle="round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
+
+def plotNode(nodeText, centerPt, parentPt, nodeType):
+  createPlot.ax1.annotate(nodeText, xy=parentPt, xycoords='axes fraction',
+  xytext=centerPt, textcoords='axes fraction',
+  va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
+
+def createPlot():
+  fig = plt.figure(1, facecolor='white')
+  fig.clf()
+  createPlot.ax1 = plt.subplot(111, frameon=False)
+  plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
+  plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
+  plt.show()
+
+def getNumLeafs(myTree):
+  numLeafs = 0
+  firstStr = myTree.keys()[0]
+  secondDict = myTree[firstStr]
+  for key in secondDict.keys():
+    if isinstance(secondDict[key], dict):
+      numLeafs += getNumLeafs(secondDict[key])
+    else:
+      numLeafs = 1
+  return numLeafs
+
+def getTreeDepth(myTree):
+  maxDepth = 0
+  firstStr = myTree.keys()[0]
+  secondDict = myTree[firstStr]
+  for key in secondDict.keys():
+    if isinstance(secondDict[key], dict):
+      thisDepth = 1 + getTreeDepth(secondDict[key])
+    else:
+      thisDepth = 1
+  if thisDepth > maxDepth : 
+    maxDepth = thisDepth
+  return maxDepth
+
+def plotMidText(cntrPt, parentPt, txtString):
+  xMid = (parentPt[0]-cntrPt[0]/2.0 + cntrPt[0])
+  yMid = (parentPt[1]-cntrPt[1]/2.0 + cntrPt[1])
+  createPlot.ax1.text(xMid, yMid, txtString)
+
+def plotTree(myTree, parentPt, nodeTxt):
+  numLeafs = getNumLeafs(myTree)
+  depth = getTreeDepth(myTree)
+  firstStr = myTree.keys()[0]
+  cntrPt = (plotTree.xOff+(1.0+float(numLeafs))/2.0/plotTree.totalW, \
+    plotTree.yOff)
+  plotMidText(cntrPt, parentPt, nodeTxt)
+  plotNode(firstStr, cntrPt, parentPt, decisionNode)
+  secondDict = myTree[firstStr]
+  plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+  for key in secondDict.keys():
+    if isinstance(secondDict[key], dict):
+      plotTree(secondDict[key].cntrPt, str(key))
+    else:
+      plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+      plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff),
+        cntrPt, leafNode)
+      plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+  plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
+def createPlot(inTree):
+  fig = plt.figure(1, facecolor = 'white')
+  fig.clf()
+  axprops = dict(xticks=[], yticks=[])
+  createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+  plotTree.totalW = float(getNumLeafs(inTree))
+  plotTree.totalD = float(getTreeDepth(inTree))
+  plotTree.xOff = -0.5/plotTree.totalW
+  plotTree.yOff = 1.0
+  plotTree(inTree, (0.5,1.0), '')
+  plt.show()
+
+#createPlot()
+
+#trainingSet, labels = CreateDataSet()
+#tree = createTree(trainingSet, copy.deepcopy(labels))
+#testSet = [0,0]
+#print(classfy(tree, labels, testSet))
+#print(classfy(tree, labels, [1,0]))
+#print(classfy(tree, labels, [1,1]))
+#print(classfy(tree, labels, [0,1]))
